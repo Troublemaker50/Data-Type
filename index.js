54a05a5a1c4d783712c131a26785cbd7b1c60a4c
Troublemaker50 <chiamakaemma77@gmail.com>
2022-07-21 14:09:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log('love you')
+let Chiamaka ="Chiamaka"+ " "+"Ugwuodo"
+console.log(Chiamaka)
+let word= 2+2
+console.log(word)
+ 
+let x=2
+let y=4
+let z=3
+let add=x+y
+let subtract=y-x
+let divide=y/x 
+let multiply=y*x
+console.log(add)
+console.log(subtract)
+console.log(divide)
+console.log(multiply)
+let equals=x==y
+console.log(equals)
+let notequals=x!=y
+console.log(notequals)
+let noequals=x!=y!=z
+console.log(noequals)
+let greaterthan=x>y
+console.log(greaterthan)
+let lessthan=x<y
+console.log(lessthan)
+let win
+console.log(win)
+let get
+console.log(get)
+let product=z*x*y
+console.log(product)
